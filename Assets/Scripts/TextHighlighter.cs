8fbed52e65612cb20ea4db102b5ebf717682efdc
Sagie Levy <sagie@lightricks.com>
2021-05-31 20:20:00 +0300

    Office: add text highlighter for all headlines in accordions.

---
+++ Assets/Scripts/TextHighlighter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextHighlighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Text uiText;
+    private Color originalTextColor;
+
+    public Color highlightedTextColor = Color.white;
+
+    private void Start()
+    {
+        uiText = GetComponent<Text>();
+        originalTextColor = uiText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        uiText.color = highlightedTextColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        uiText.color = originalTextColor;
+    }
+}
