8ae94516febe40ac56f293cd0a89b52e4095b615
Sagie Levy <sagie@lightricks.com>
2021-06-04 18:28:35 +0300

    Murder 1: add unknown evidence pages with event to turn to revealed evidence pages. Refactored image assets locations.

---
+++ Assets/Scripts/Custom Controllers/EvidencePhotoController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using AC;
+
+public class EvidencePhotoController : MonoBehaviour
+{
+    [SerializeField] private GameObject questionMark;
+    [SerializeField] private GameObject evidencePhoto;
+
+    [SerializeField] private GameObject unknownTextHeadline;
+    [SerializeField] private GameObject evidenceTextHeadline;
+
+    [SerializeField] private GameObject unknownTextContent;
+    [SerializeField] private GameObject evidenceTextContent;
+
+    [SerializeField] private string isEvidenceRevealedVarLabel;
+
+    private void Start()
+    {
+        var isEvidenceRevealed = GlobalVariables
+            .GetVariable(isEvidenceRevealedVarLabel).BooleanValue;
+
+        questionMark.SetActive(!isEvidenceRevealed);
+        unknownTextHeadline.SetActive(!isEvidenceRevealed);
+        unknownTextContent.SetActive(!isEvidenceRevealed);
+
+        evidencePhoto.SetActive(isEvidenceRevealed);
+        evidenceTextHeadline.SetActive(isEvidenceRevealed);
+        evidenceTextContent.SetActive(isEvidenceRevealed);
+    }
+}
