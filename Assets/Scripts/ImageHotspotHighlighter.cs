4b55425159faa6e3af489d7cc65fbb892b639fa8
Sagie Levy <sagie@lightricks.com>
2021-06-19 16:16:16 +0300

    Office: highlight photos on hover.

---
+++ Assets/Scripts/ImageHotspotHighlighter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageHotspotHighlighter : MonoBehaviour
+{
+    private Image image;
+
+    private Color originalOutlineColor;
+
+    public Color highlightOutlineColor = Color.white;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+
+        originalOutlineColor = image.color;
+    }
+
+    public void ObjectHighlightOn()
+    {
+        image.color = highlightOutlineColor;
+    }
+
+    public void ObjectHighlightOff()
+    {
+        image.color = originalOutlineColor;
+    }
+}
