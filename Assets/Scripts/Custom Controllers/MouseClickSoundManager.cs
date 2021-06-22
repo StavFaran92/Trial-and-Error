9fc466848c3c1a49aeacbf928463b7239347f0a5
Sagie Levy <sagie@lightricks.com>
2021-06-22 20:13:15 +0300

    Crime Scene 2: revert door SFX. Project: add mouse click sound manager to all scenes.

---
+++ Assets/Scripts/Custom Controllers/MouseClickSoundManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MouseClickSoundManager: MonoBehaviour
+{
+    private AudioSource mouseClickSound;
+
+    void Start()
+    {
+        mouseClickSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !mouseClickSound.isPlaying)
+        {
+            mouseClickSound.Play();
+        }
+    }
+}
