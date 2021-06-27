c273463808bdf0a8f1e9be7bc77c01b17b235ff8
Sagie Levy <sagie@lightricks.com>
2021-06-27 21:28:41 +0300

    TextMeshProHighlighter: initial commit.

---
+++ Assets/Scripts/TextMeshProHighlighter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextMeshProHighlighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    private TextMeshProUGUI text;
+    private Color originalTextColor;
+
+    public Color highlightedTextColor = Color.white;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        originalTextColor = text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = highlightedTextColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = originalTextColor;
+    }
+}
+
